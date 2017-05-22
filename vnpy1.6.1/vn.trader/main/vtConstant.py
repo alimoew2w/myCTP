68a84589957c4feb0d18bf451ca8d96fe56bbb37
williamlfang <william.lian.fang@gmail.com>
2017-05-22 15:01:06 +0800

    修改 /main/vtConstant.py 路径

--- vnpy1.6.1/vn.trader/main/vtConstant.py
+++ vnpy1.6.1/vn.trader/main/vtConstant.py
@@ -1,13 +1,9 @@
 # encoding: UTF-8
 
-################################################################################
+# ################################################################################
 ## william
-## from language import constant
-language_path = '/home/william/Documents/vnpy/vnpy-1.6.1/vn.trader/language/chinese'
-import sys
-sys.path.append(language_path)
-import constant
-################################################################################
+from language import constant
+# ################################################################################
 
 # 将常量定义添加到vtConstant.py的局部字典中
 d = locals()
