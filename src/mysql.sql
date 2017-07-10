c509000d66fec6e877d0922181a3c97d0647d112
williamlfang <william.lian.fang@gmail.com>
2017-07-10 14:02:22 +0800

    从 positionInfo 删除 tradeTime

--- src/mysql.sql
+++ src/mysql.sql
@@ -6,7 +6,6 @@ create table positionInfo(
     strategyID      VARCHAR(100) NOT NULL,
     InstrumentID    VARCHAR(20)  NOT NULL,
     TradingDay      DATE         NOT NULL,
-    tradeTime       DATETIME     NOT NULL,
     direction       VARCHAR(20)  NOT NULL,
     volume          INT          NULL,
     PRIMARY KEY(strategyID, InstrumentID, TradingDay, direction)
@@ -25,8 +24,8 @@ create table tradingInfo(
     offset          VARCHAR(20)  NOT NULL,
     volume          INT           NOT NULL,
     price           DECIMAL(15,5) NOT NULL,
-    -- PRIMARY KEY(strategyID, InstrumentID, TradingDay, tradeTime, direction, offset)   
-    PRIMARY KEY(strategyID, InstrumentID, TradingDay, direction, offset)   
+    -- PRIMARY KEY(strategyID, InstrumentID, TradingDay, tradeTime, direction, offset)
+    PRIMARY KEY(strategyID, InstrumentID, TradingDay, tradeTime, direction, offset)
 );
 
 ## orderTime: 下单时间
@@ -44,5 +43,5 @@ create table failedInfo(
     direction       VARCHAR(20)  NOT NULL,
     offset          VARCHAR(20)  NOT NULL,
     volume          INT           NOT NULL,
-    PRIMARY KEY(strategyID, InstrumentID, TradingDay, direction, offset)   
+    PRIMARY KEY(strategyID, InstrumentID, TradingDay, direction, offset)
 );
