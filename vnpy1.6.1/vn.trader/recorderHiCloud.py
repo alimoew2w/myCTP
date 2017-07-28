4bd321c23a3d56821c1bf5fab16c077b36a9cdda
williamlfang <william.lian.fang@gmail.com>
2017-07-28 20:23:37 +0800

    完成 recorderHiCloud 数据记录程序

--- vnpy1.6.1/vn.trader/recorderHiCloud.py
+++ vnpy1.6.1/vn.trader/recorderHiCloud.py
@@ -3,7 +3,8 @@
 ## william
 ## 用于记录 CTP Tick Data
 ################################################################################
-
+ROOT_PATH = "/home/william/Documents/myCTP/vnpy1.6.1/vn.trader"
+# DATA_PATH = "/media/william/Storage2/vnpyData"
 ################################################################################
 ##　william
 ## 加载包
@@ -15,14 +16,25 @@ import platform
 import re
 from datetime import datetime
 import time
-################################################################################
-
-
-################################################################################
 import csv
+import json
+
+SETTING_FILE = os.path.normpath(os.path.join(ROOT_PATH,'main/setting','VT_setting.json'))
+MAIN_SETTING = json.load(file(SETTING_FILE))
+################################################################################
+
+################################################################################
+## 添加路径
+################################################################################
+MAIN_PATH = os.path.normpath(os.path.join(ROOT_PATH, 'main'))
+sys.path.append(MAIN_PATH)
+os.chdir(MAIN_PATH)
+################################################################################
+
+################################################################################
 # path = os.path.dirname(__file__)
-path = '/home/william/Documents/myCTP/vnpy1.6.1/vn.trader'
-ChinaFuturesCalendar = os.path.normpath(os.path.join(path,'main','ChinaFuturesCalendar.csv'))
+# path = ROOT_PATH
+ChinaFuturesCalendar = os.path.normpath(os.path.join(ROOT_PATH,'main','ChinaFuturesCalendar.csv'))
 TradingDay = []
 with open(ChinaFuturesCalendar) as f:
     ChinaFuturesCalendar = csv.reader(f)
@@ -39,25 +51,33 @@ if datetime.now().strftime("%Y%m%d") not in TradingDay:
 
 ################################################################################
 ##　增加路径说明
-################################################################################
-ROOT_PATH = "/home/william/Documents/myCTP/vnpy1.6.1/vn.trader"
-main_path = os.path.normpath(os.path.join(ROOT_PATH, 'main'))
-data_recorder_path = "/home/william/Documents/myCTP/vnpy1.6.1/vn.data"
-# data_recorder_path = "/media/william/Storage2/myData"
-
-sys.path.append(main_path)
-os.chdir(main_path)
-################################################################################
 ##　william
 import vtPath
 import eventEngine, vtEngineRecorder, vtGateway
 from vtEngineRecorder import MainEngine
 import vtFunction
 
+################################################################################
+## ========================== 开始启动主程序 ================================== ##
+################################################################################
+"""主程序入口"""
+# 初始化主引擎和主窗口对象
+############################################################################
+## william
+## 继承 vtEngine::MainEngine
+mainEngine = MainEngine()
+mainEngine.DATA_PATH = MAIN_SETTING['DATA_PATH']
+############################################################################
+print "\n#######################################################################"
+print "main 主函数启动成功！！！"
+time.sleep(1.0)
+print "#######################################################################\n"
+############################################################################
+
+
 ## /////////////////////////////////////////////////////////////////////////////
 ## 保存 Tick Data 为 /data/csv
-import csv
-dataFile = os.path.join(data_recorder_path,'TickData', (datetime.now().strftime('%Y%m%d')+ '.csv'))
+dataFile = os.path.join(mainEngine.DATA_PATH,'TickData', (datetime.now().strftime('%Y%m%d')+ '.csv'))
 
 if not os.path.exists(dataFile):
     myHeader = ['timeStamp','date','time','symbol','exchange',\
@@ -76,52 +96,16 @@ if not os.path.exists(dataFile):
     f.close()
 ## /////////////////////////////////////////////////////////////////////////////
 
-
-################################################################################
-## ========================== 开始启动主程序 ================================== ##
-################################################################################
-"""主程序入口"""
-# 初始化主引擎和主窗口对象
-############################################################################
-## william
-## 继承 vtEngine::MainEngine
-mainEngine = MainEngine()
-
-############################################################################
-## william
-## 增加 “启动成功” 的提示。
-##'''
-# mainWindow = MainWindow(mainEngine, mainEngine.eventEngine)
-# ## mainWindow.showMaximized()
-# mainWindow.showMinimized()
-##'''
-print "\n#######################################################################"
-print u"main 主函数启动成功！！！"
-time.sleep(1.0)
-print "#######################################################################\n"
-############################################################################
-
-
 ############################################################################
 ## william
 ## 默认启动连接 CTP
 gatewayName = 'CTP'
-print U"GatewayName:", gatewayName
+print "GatewayName:", gatewayName
 
 try:
     mainEngine.connectCTPAccount(accountInfo = 'HiCloud')
-    print u"CTP 正在登录!!!",
-    for i in range(20):
-        print ".",
-        time.sleep(.1)
-
-    print "\n#---------------------------------------------------------------"
-    print u"CTP 连接成功!!!"
-    print "#---------------------------------------------------------------"
 except:
-    print "#---------------------------------------------------------------"
-    print u"CTP 连接失败!!!"
-    print "#---------------------------------------------------------------"
+    sys.exit("Failed to connect to CTP!!!")
 ################################################################################
 mainEngine.saveContractInfo()
 
@@ -147,12 +131,12 @@ df = pd.DataFrame(dfData, columns = dfHeader)
 reload(sys) # reload 才能调用 setdefaultencoding 方法
 sys.setdefaultencoding('utf-8')
 
-df.to_csv(os.path.join(data_recorder_path,'ContractInfo', ('ContractInfo_' + datetime.now().strftime('%Y%m%d') + '.csv')), index = False)
+df.to_csv(os.path.join(mainEngine.DATA_PATH,'ContractInfo', ('ContractInfo_' + datetime.now().strftime('%Y%m%d') + '.csv')), index = False)
 ################################################################################
 
 
 ## /////////////////////////////////////////////////////////////////////////////
 print "\n#######################################################################"
-print u"正在下载 CTP Tick Data !!!"
+print "正在下载 CTP Tick Data !!!"
 print "#######################################################################\n"
 ## /////////////////////////////////////////////////////////////////////////////
