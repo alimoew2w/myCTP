b03030e20e4c9d55b49ddd316ab5da0bc8eea895
williamlfang <william.lian.fang@gmail.com>
2017-12-11 15:01:20 +0800

    增加 globalSetting 为 class

--- vnpy-1.7.2/vnpy/trader/vtGlobal.py
+++ vnpy-1.7.2/vnpy/trader/vtGlobal.py
@@ -4,10 +4,40 @@
 通过VT_setting.json加载全局配置
 """
 
-import os
+## =============================================================================
+import sys,os,subprocess
+os.putenv('DISPLAY', ':0.0')
+reload(sys)
+sys.setdefaultencoding('utf8')
+## =============================================================================
+
 import traceback
 import json
 from .vtFunction import getJsonPath
+
+## =============================================================================
+# 判断操作系统
+import re,datetime,time,csv
+from datetime import datetime,time
+## =============================================================================
+
+## =============================================================================
+## william
+##　判断当天是否有交易
+## -----------------------------------------------------------------------------
+TradingDay = []
+with open('./trading/ChinaFuturesCalendar.csv') as f:
+    ChinaFuturesCalendar = csv.reader(f)
+    for row in ChinaFuturesCalendar:
+        if row[1] >= '20170101':
+            TradingDay.append(row[1])
+TradingDay.pop(0)
+
+if datetime.now().strftime("%Y%m%d") not in TradingDay:
+    print '#'*80
+    sys.exit("启禀圣上，今日赌场不开张!!!")
+    print '#'*80
+## =============================================================================
 
 
 settingFileName = "setting/VT_setting.json"
