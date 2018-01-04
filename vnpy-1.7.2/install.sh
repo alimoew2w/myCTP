34bc5053574b67494c7744fc1632d4274532bb09
williamlfang <william.lian.fang@gmail.com>
2018-01-04 14:14:55 +0800

    只保留 ctp 接口

--- vnpy-1.7.2/install.sh
+++ vnpy-1.7.2/install.sh
@@ -5,17 +5,17 @@ pushd vnpy/api/ctp
 bash build.sh
 popd
 
-pushd vnpy/api/lts
-bash build.sh
-popd
+# pushd vnpy/api/lts
+# bash build.sh
+# popd
 
-pushd vnpy/api/xtp
-bash build.sh
-popd
+# pushd vnpy/api/xtp
+# bash build.sh
+# popd
 
-pushd vnpy/api/ib
-bash build.sh
-popd
+# pushd vnpy/api/ib
+# bash build.sh
+# popd
 
 #Install Ta-Lib
 conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
